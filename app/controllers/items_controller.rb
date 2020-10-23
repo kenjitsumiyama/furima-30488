614bc7daa50f536d646ea4edd60c9eecab5e9d44
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2020-10-23 21:23:16 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, only: [:edit]
   def index  
     @items = Item.all.order(id: "DESC")
   end
@@ -8,40 +9,46 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  
-    def create
-      @item = Item.new(item_params)
-    
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def show
-      @item = Item.find(params[:id])
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
     end
-
-    def edit
-      
-    end
-
-    def update
-      
-    end
-
-    def destroy
-      
-    end
-
-  
-
+  end
 
   private
   def item_params
     params.require(:item).permit(:name, :info,:category_id,:condition_id,:shipping_id,:prefecture_id,:scheduled_delivery_id,:price,:image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    item = Item.find(params[:id])
+  unless 
+     user_signed_in? && current_user.id == item.user.id
+     redirect_to action: :index
+  end
+ end
 end
 
