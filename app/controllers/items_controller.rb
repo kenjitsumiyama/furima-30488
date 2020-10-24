580624c0bc10ab44025fb2f71409e1ca880af9b8
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2020-10-24 18:32:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,8 +45,8 @@ class ItemsController < ApplicationController
   def move_to_index
     item = Item.find(params[:id])
   unless 
-     user_signed_in? && current_user.id == item.user.id
-     redirect_to action: :index
+    user_signed_in? && current_user.id == item.user.id
+    redirect_to action: :index
   end
  end
 
